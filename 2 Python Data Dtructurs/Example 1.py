6da667855eeca2948e8514fc75d772224bfca282
PankajJadhav18 <pankajjadhavfsd@gmail.com>
2025-08-28 23:12:14 +0530

    Add files via upload

---
+++ 2 Python Data Dtructurs/Example 1.py
@@ -0,0 +1,16 @@
+# Dictionary
+Student_Marks = {
+    "Pankaj":74,
+    "Mahesh":83,
+    "Ajit":78,
+    "Sunil":72
+}
+total=sum(Student_Marks.values())
+avarage=total/4
+print(avarage)
+
+
+# List
+Bikes = ["GT-650", "HF-Deluxe", "Kawasaki", "RX-100", "HF-Deluxe"]
+set = list(set(Bikes))
+print(set)
