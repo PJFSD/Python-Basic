fb2cb86ccc2f49191cf5dfc8d31c4aa484b9258d
PankajJadhav18 <pankajjadhavfsd@gmail.com>
2025-10-05 21:33:04 +0530

    Add files via upload

---
+++ Q3.py
@@ -0,0 +1,8 @@
+# Given a list of numbers, return a new list with only the even numbers.
+
+
+
+numbers = [56,21,22,23,24,25,46,88,100]
+even_numbers = [num for num in numbers if num % 2 == 0]
+
+print(even_numbers)
