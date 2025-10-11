ce883666eaabae41c817362cc3fe9b5bfb3e0eac
PankajJadhav18 <pankajjadhavfsd@gmail.com>
2025-10-11 23:19:06 +0530

    Add files via upload

---
+++ Q7.py
@@ -0,0 +1,7 @@
+#Write a program to count how many times a value appears in a tuple.
+
+boys = ("manoj", "vivek", "vinayak", "mahesh", "sumit", "rahul", "rohit")
+value_to_count = "vinayak"
+
+count = (boys, value_to_count)
+print(f"The value {value_to_count} appears {count} times.")
