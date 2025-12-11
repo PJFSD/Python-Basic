72a8cb1482ffdc3e42cf6a00a3c42f6b5d4c4f73
PankajJadhav18 <pankajjadhavfsd@gmail.com>
2025-12-11 22:49:35 +0530

    Add files via upload

---
+++ home test python/Dictionary (dict).py
@@ -0,0 +1,10 @@
+# Key-Value Paire
+# student = {"name": "Pankaj", "age": 21}
+# print(type(student))       # <class 'dict'>
+
+# Q1. Output?
+person = {"name": "John", "age": 30}
+print(person["name"])
+person["age"] = 35
+print(person)
+
