eb10d1ea366bfce2d2f64b7c173686fcaf6f3052
PankajJadhav18 <pankajjadhavfsd@gmail.com>
2025-12-11 22:47:59 +0530

    Add files via upload

---
+++ home test python/Float.py
@@ -0,0 +1,11 @@
+# Numbers with decimals.
+# price = 19.99
+# print(type(price))  # <class 'float'>
+
+
+# Q2. What will print(num * 2) output?
+# num = 5.5
+# print(num * 2)
+
+num = 41.7
+print(num - 6)
