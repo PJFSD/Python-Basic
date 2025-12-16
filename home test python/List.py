f5bb995762448221fab82b9270bea6f723ceb8c6
PankajJadhav18 <pankajjadhavfsd@gmail.com>
2025-12-16 22:40:55 +0530

    Add files via upload

---
+++ home test python/List.py
@@ -0,0 +1,11 @@
+# Ordered, changeable collection.
+# fruits = ["apple", "banana", "mango"]
+# print(type(fruits))        # <class 'list'>
+
+
+# Q1. Predict the output.
+nums = [10, 20, 30, 40]
+nums.append(50)
+print(nums[2])
+print(nums)
+
