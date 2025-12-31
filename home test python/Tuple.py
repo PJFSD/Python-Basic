0c90538d35163c096752fb2c93c072f58d094cfd
PankajJadhav18 <pankajjadhavfsd@gmail.com>
2025-12-31 23:11:46 +0530

    Add files via upload

---
+++ home test python/Tuple.py
@@ -0,0 +1,8 @@
+# Ordered, unchangeable collection.
+# colors = ("red", "blue", "green")
+# print(type(colors))        # <class 'tuple'>
+
+# Q1. What happens here?
+t = (1, 2, 3)
+t[1] = 5           # TypeError: 'tuple' object does not support item assignment 
+
